41bb0ac70e28ed30a7e962f6d3e34b951de3adba
bxiehq <bxiehq@users.noreply.github.com>
2026-08-10 20:33:24 +0800

    Import go-to-definition selection handler

--- Src/VsVimShared/VsVimHost.cs
+++ Src/VsVimShared/VsVimHost.cs
@@ -16,6 +16,7 @@ using Microsoft.VisualStudio.Utilities;
 using Vim;
 using Vim.Extensions;
 using Vim.UI.Wpf;
+using Vim.VisualStudio.Implementation.Misc;
 using Microsoft.VisualStudio.Shell.Interop;
 using Microsoft.VisualStudio;
 using Microsoft.FSharp.Core;
